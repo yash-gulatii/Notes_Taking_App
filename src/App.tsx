3bd4b10edf50a710040bd52093f948e3e30c9cbd
Yash <contactyashgulati@gmail.com>
2023-03-21 14:19:52 +0530

    Project Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useMemo } from "react";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./NewNote";
+import { NoteList } from "./NoteList";
+import { NoteLayout } from "./NoteLayout";
+import { useLocalStorage } from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
+import { Note } from "./Note";
+import { EditNote } from "./EditNote";
 
-function App() {
-  const [count, setCount] = useState(0)
+export type Note = {
+  id: string
+} & NoteData
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
 }
 
-export default App
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [...prevNotes,
+      { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+      ]
+    })
+  }
+
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return prevNotes.map(note => {
+        if (note.id === id) {
+          return { ...note, ...data, tagIds: tags.map(tag => tag.id) }
+        } else {
+          return note
+        }
+      })
+    })
+  }
+
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
+  function addTag(tag: Tag) {
+    setTags(prev => [...prev, tag])
+  }
+
+  function updateTag(id: string, label: string) {
+    setTags(prevTags => {
+      return prevTags.map(tag => {
+        if (tag.id === id) {
+          return { ...tag, label }
+        } else {
+          return tag
+        }
+      })
+    })
+  }
+
+  function deleteTag(id: string) {
+    setTags(prevTags => {
+      return prevTags.filter(tag => tag.id !== id)
+    })
+  }
+
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags} onUpdateTag={updateTag} onDeleteTag={deleteTag} />} />
+        <Route path="/new" element={<NewNote
+          onSubmit={onCreateNote}
+          onAddTag={addTag}
+          availableTags={tags} />} />
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />} >
+          <Route index element={<Note onDelete={onDeleteNote} />} />
+          <Route path="edit" element={<EditNote onSubmit={onUpdateNote}
+            onAddTag={addTag}
+            availableTags={tags} />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Note.tsx
@@ -0,0 +1,44 @@
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
+import { Link, useNavigate } from "react-router-dom";
+import { useNote } from "./NoteLayout";
+
+type NoteProps = {
+    onDelete: (id: string) => void
+}
+
+export function Note({ onDelete }: NoteProps) {
+    const note = useNote()
+    const navigate = useNavigate()
+
+    return <>
+        <Row className="align-items-center mt-5  mb-4">
+            <Col>
+                <h1>{note.title}</h1>
+                {note.tags.length > 0 && (
+                    <Stack gap={1} direction="horizontal" className="flex-wrap">
+                        {note.tags.map(tag => (
+                            <Badge className="text-truncate" key={tag.id} >{tag.label}</Badge>
+                        ))}
+                    </Stack>
+                )}
+            </Col>
+            <Col xs="auto">
+                <Stack gap={2} direction="horizontal">
+                    <Link to={`/${note.id}/edit`} >
+                        <Button className="m-1" variant="primary" > Edit </Button>
+                    </Link>
+                    <Button onClick={() => {
+                        onDelete(note.id)
+                        navigate("/")
+                    }} className="m-1" variant="outline-danger" > Delete </Button>
+                    <Link to="/" >
+                        <Button className="m-1" variant="outline-secondary" > Back </Button>
+                    </Link>
+
+                </Stack>
+            </Col>
+        </Row>
+        <ReactMarkdown>{note.markdown}</ReactMarkdown>
+    </>
+}
